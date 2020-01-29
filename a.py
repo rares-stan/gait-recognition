2b1aefa0de8375df27f31c8e22c9187267b6fe30
Stan Rares Alexandru <stan.rares@gmail.com>
2020-01-29 23:28:59 +0200

    aa

--- a.py
+++ a.py
@@ -12,5 +12,14 @@ input = 'data/separate-test/validation-090-test-80-frames.pickle'
 with open(input, 'rb') as inp:
     data = pickle.load(inp)
 
-with open('data/separate-test/one-set.pickle', 'wb') as out:
-    pickle.dump(data[2], out)
+first = data[0][0]
+
+n = len(first)
+
+io.imsave("90-1.jpg", first[0])
+io.imsave("90-2.jpg", first[n//3])
+io.imsave("90-3.jpg", first[2*(n//3)])
+io.imsave("90-4.jpg", first[n-1])
+
+# with open('data/separate-test/one-set.pickle', 'wb') as out:
+#     pickle.dump(data[2], out)
